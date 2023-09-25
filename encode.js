03749784a29f760c99fcd90ce96d40f57baa0aa3
Boycey7 <jake.boyce7@hotmail.com>
2023-09-25 16:02:51 +0100

    encode.js code and jest tests

--- encode.js
+++ encode.js
@@ -1,3 +1,25 @@
-function encode() {
-    
-}
+//Run tests to check the function
+
+// Caesar Cipher Implementation
+function caesarCipher(str, shift) {
+    let result = "";
+  
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i]; // Get the current character in the input string
+  
+      if (char.match(/[a-zA-Z]/)) {
+        // determine the shifted character
+        const charCodeStart = char.toLowerCase() === char ? 97 : 65;
+        const charIndex = char.charCodeAt(0) - charCodeStart;
+        const shiftedIndex = ((charIndex + shift % 26) + 26) % 26;  // Calculate the shifted index
+        char = String.fromCharCode(charCodeStart + shiftedIndex);  // Get the shifted character
+      }
+  
+      result += char;  // Append the character to the result
+    }
+  
+    return result;  // Return the shifted text
+  }
+  
+  module.exports = caesarCipher;  // Export the caesarCipher function
+  
